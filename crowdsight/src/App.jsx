aa385d6606a65c529d628f4626d7944811a6a043
myn-chin <145184302+myn-chin@users.noreply.github.com>
2024-08-29 13:29:35 +0000

    hello

---
+++ crowdsight/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from 'pages/Home.jsx'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        path="/"
+      >
+        <Route index element={<Home />} />
+      </Route>,
+    ),
+  );
+
+  <>
+    <RouterProvider router={router} />
+  </>
+}
+
+export default App
