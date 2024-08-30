db52aa29d8cb5e8a64560123b931d0b4552da06d
saplingonfire <minhan.ho.2023@scis.smu.edu.sg>
2024-08-30 19:23:48 +0800

    heatmap page

--- crowdsight/src/pages/Heatmap.jsx
+++ crowdsight/src/pages/Heatmap.jsx
@@ -1,6 +1,28 @@
+import axios from "axios";
+import React from "react";
+
 function Heatmap(){
+    const backend_endpoint = import.meta.env.VITE_BACKEND_ENDPOINT + "/create_map";
+
+    const config = {
+        method: 'get',
+        url: backend_endpoint
+    }
+
+    axios(config)
+    .then((response) => {
+        if (response.data !== null) {
+            console.log("heatmap successfully retrieved");
+            const map_html = response.data;
+        } else {
+            alert("failed to retrieve heatmap");
+        }
+    })
+
     return(
-        <>hello</>
+        <>
+            <embed type="text/html" src={map_html} width="1200" height="800"></embed>
+        </>
     )
 }
 export default Heatmap
